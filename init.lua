72c364f82b37b8c2da2bb2deebb82add67e3e842
Jamtoad <65873272+Jamtoad@users.noreply.github.com>
2022-11-12 23:18:08 -0500

    Adds links!

--- init.lua
+++ init.lua
@@ -1,18 +1,6 @@
---[[
-    Bullfrog
-    Version 1
-    11/04/2022
-
-    API
-        getSystem()
-        setupSystems()
-        start()
-    
-    Lifetime Functions
-        onSetup()
-        onStart()
-        onUpdate()
-]]
+-- Bullfrog | Version 2
+-- Version 1: Initial release. | 11/07/2022
+-- Version 2: Links and comments added! | 11/12/2022
 
 -- Root
 local BULLFROG = {}
@@ -24,55 +12,117 @@ local RUN_SERVICE = game:GetService("RunService")
 local IS_SERVER = RUN_SERVICE:IsServer()
 
 -- Variables
-local _systems = {}
+local systems = {}
 
+-- Global Functions
+--[[
+    Description - Creates a remote event
+    Returns - The created remote event
+]]
+BULLFROG.createRemoteLink = if IS_SERVER then function()
+    return Instance.new("RemoteEvent")
+end else nil
+
+--[[
+    Description - Creates a bindable event
+    Returns - The created bindable event
+]]
+function BULLFROG.createBindableLink()
+    return Instance.new("BindableEvent")
+end
+
+--[[
+    Description - Gets the requested system within the same environment
+    Parameters - [string] The systems name
+    Returns - The requested system
+    Errors - If no requested system is specified
+]]
 function BULLFROG.getSystem(requestedSystem)
     if not requestedSystem then
         error("Did not specify a system to get.")
     end
 
-    if not _systems[requestedSystem] then
+    if not systems[requestedSystem] then
         error("The specified system was not found" .. requestedSystem)
     end
 
-    return _systems[requestedSystem]
+    return systems[requestedSystem]
 end
 
+--[[
+    Description - Sets up the systems within the specified systems directory,
+    sets up any remote links or bindable links, and calls onSetup()
+    Parameters - [object] The directory to search for systems in
+    Errors - If no directory is specified
+]]
 function BULLFROG.setupSystems(systemsDirectory)
+    local function setupLinks(system, links)    
+        local function setupFolder()
+            local _folder = Instance.new("Folder")
+            _folder.Name = "Links"
+            _folder.Parent = system
+            
+            return _folder
+        end
+        
+        for name, link in pairs(links) do
+            link.Name = name
+            link.Parent = system:FindFirstChild("Links") or setupFolder()
+        end
+        
+        return nil
+    end
+
     if not systemsDirectory then
         error("Did not specify a systems directory.")
     end
-
+    
     for _, _system in pairs(systemsDirectory:GetChildren()) do
         local _module = _system:FindFirstChild(
-            if IS_SERVER then
-                "Server"
-            else
-                "Client"
-        )
+            if IS_SERVER then "Server" else "Client")
     
         if _module then
             local _requiredModule = require(_module)
             
-            _systems[_system.Name] = _requiredModule
+            systems[_system.Name] = _requiredModule
+            
+            if IS_SERVER and _requiredModule.remoteLinks then
+                setupLinks(_system, _requiredModule.remoteLinks)
+            elseif not IS_SERVER then
+                _requiredModule.remoteLinks = _system:FindFirstChild("Links")
+            end
+            
+            if _requiredModule.bindableLinks then
+                setupLinks(_system, _requiredModule.bindableLinks)
+            end
             
             if _requiredModule.onSetup then
                 _requiredModule.onSetup()
             end
         end
     end
+    
+    return nil
 end
 
+--[[
+    Description - Starts the systems that were setup in setupSystems(),
+    also calls onStart() and binds onUpdate()
+]]
 function BULLFROG.start()
-    for _, _system in pairs(_systems) do
+    for _, _system in pairs(systems) do
         if _system.onStart then
             coroutine.wrap(_system.onStart)()
         end
         
         if _system.onUpdate then
-            RUN_SERVICE.Heartbeat:Connect(_system.onUpdate)
+            coroutine.wrap(function()
+                RUN_SERVICE.Heartbeat:Connect(_system.onUpdate)
+            end)()
         end
     end
+    
+    return nil
 end
 
 return BULLFROG
